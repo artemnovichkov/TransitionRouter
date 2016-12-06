49dc34b2f7c772dd0f81243c7ca57bd346ff38da
Artem Novichkov <artemnovichkov@Artems-MacBook-Air.local>
2016-12-06 20:33:03 +0600

    Initial Commit

---
+++ TransitionRouter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TransitionRouter
+//
+//  Created by Artem Novichkov on 06/12/2016.
+//  Copyright © 2016 Artem Novichkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
